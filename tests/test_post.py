156455ebded1cde10fff52e992b85e8da58b40d0
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-24 21:19:23 +0300

    updated test files

--- tests/test_post.py
+++ tests/test_post.py
@@ -10,8 +10,8 @@ class PostModelTest(unittest.TestCase):
         Set up method that will run before every Test
         '''
         self.new_user = User(id=1,username='triplek',email='triplek901@gmail.com',image_file="default.png",date_joined="2021-08-23", password = 'kelvin')
-        self.new_post = Post(id=1,comment='Waaah!! this one killed me',user_id=self.new_user.id,date_posted='2021-08-20 14:58:31.689649')
-
+        self.new_post = Post(id=1,title='BBI suddenly stopped', content="BBI Court ruling today: Judgement reject Kenyatta Building Bridges Initiative to change Kenya constitution",
+                                user_id=self.new_user.id, date_posted='2021-08-20 14:58:31')
     def test_instance(self):
         self.assertTrue(isinstance(self.new_post,Post))
 
