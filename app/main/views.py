cd24a29393699281828ed3b023270497eef9c1c2
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-25 11:34:20 +0300

    updated picture path

--- app/main/views.py
+++ app/main/views.py
@@ -37,7 +37,7 @@ def save_picture(form_pic):
     random_hex=secrets.token_hex(8)
     _, f_ext=os.path.splitext(form_pic.filename)
     picture_fn = random_hex + f_ext 
-    picture_path = os.path.join('/app/static/profile_images',picture_fn)
+    picture_path = os.path.join('/static/profile_images',picture_fn)
     form_pic.save(picture_path)
     output_size=(125,125)
     i = Image.open(form_pic)
