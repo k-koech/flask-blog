41cfa5c2f9f6b48fbdcca74fcff0485ffcd2dbc0
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-25 11:42:53 +0300

    updated picture path

--- app/main/views.py
+++ app/main/views.py
@@ -56,7 +56,7 @@ def profile():
     quotes = json.loads(raw_quotes)
     if form.validate_on_submit():
         if form.picture.data:
-                picture_file="sjh.png"
+                picture_file="default.jpg"
                 current_user.image_file=picture_file
 
         current_user.username=form.username.data
