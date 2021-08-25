83041e99005c76f1d27403c25c3e23069928ebb5
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-25 09:21:21 +0300

    updated auth views

--- app/auth/views.py
+++ app/auth/views.py
@@ -46,6 +46,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
                 next_page = request.args.get('next')  
+                flash(f"Logged in successfully!", "success")
                 return redirect(next_page) if next_page else redirect(url_for("main.home"))
         else:
             flash(f"Invalid credentials", "danger")
