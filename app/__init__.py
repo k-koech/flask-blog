75f384429f577222bd03de831331d437f96a318c
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-25 05:01:12 +0300

    updated email

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,6 @@ def create_app(config_name):
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = 465,
     MAIL_USE_TLS = False,
-    MAIL_USE_SSL = True,
     MAIL_USERNAME = "kalambanidouglas@gmail.com",
     MAIL_PASSWORD ="kalambani97?")
     
--- config.py
+++ config.py
@@ -25,7 +25,6 @@ class ProdConfig(Config):
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
     MAIL_USERNAME = "kalambanidouglas@gmail.com"
     MAIL_PASSWORD ="kalambani97?"
 
