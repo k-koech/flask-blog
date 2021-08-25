780ac59ff0462eaf168ac21f00fc1e699f385ce7
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-25 09:17:14 +0300

    updated email

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='vsegsetge3'
 export MAIL_USERNAME="kalambanidouglas@gmail.com"
-export MAIL_PASSWORD="kalambani97?"
+export MAIL_PASSWORD="kalambani97!"
 
 python3 manage.py server
